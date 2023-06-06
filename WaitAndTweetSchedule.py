d40dbc6c3e22e0cf7740b777e369be9a67623446
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-06-06 22:14:34 +0900

    WaitAndTweetSchedule.py の更新

--- WaitAndTweetSchedule.py
+++ WaitAndTweetSchedule.py
@@ -379,7 +379,7 @@ while True:
                 print(str(e))
             IsBreak = True
        if IsEvent:
-           if datetime.strptime(schjson["data"]["eventSchedules"]["nodes"][0]["timePeriods"][-1]["endTime"], '%Y-%m-%dT%H:%M:%SZ') > datetime.utcnow() and schjson["data"]["eventSchedules"]["nodes"][-1]["timePeriods"][-1]["startTime"] is not lasteventrule:
+           if len(schjson["data"]["eventSchedules"]["nodes"])>1 and  datetime.strptime(schjson["data"]["eventSchedules"]["nodes"][0]["timePeriods"][-1]["endTime"], '%Y-%m-%dT%H:%M:%SZ') > datetime.utcnow() and schjson["data"]["eventSchedules"]["nodes"][-1]["timePeriods"][-1]["startTime"] is not lasteventrule:
             transjson = requests.get("https://splatoon3.ink/data/locale/ja-JP.json").json()
             CreateEventImage(schjson["data"]["eventSchedules"]["nodes"][-1]).save("event.png")
             tweettext =  "新しいイベントマッチスケジュールが公開されました！\n"
